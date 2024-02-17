f895e69156bab7ae43678e8365e3a4276a2e963f
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2024-02-17 12:05:55 +0000

    gfrefdjitwedskrwoftck,plht;gpg
    grs;gr'fdcxlphsgrdxl;pfjmptylnhgf;vbc

--- WAV OCS AIRPORT EDITION/Assets/Scripts/levmun.cs
+++ WAV OCS AIRPORT EDITION/Assets/Scripts/levmun.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class levmun : MonoBehaviour
 {
- 
+
     public void OnclickLevel(int levelNum)
     {
         SceneManager.LoadScene("Gamescene");
     }
     public void OnClickBack()
     {
-    this.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
     }
     // Start is called before the first
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+
+    }
+    public void back2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("quit?");
+        Application.Quit();
+    }
+
+
 
 }
-
