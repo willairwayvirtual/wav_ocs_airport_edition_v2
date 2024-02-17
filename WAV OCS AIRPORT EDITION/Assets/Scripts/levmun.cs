c98390ac702ae67d2fa4e0c8dd4e2874c14da8a2
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2024-02-17 11:51:11 +0000

    sdg;wrsdggrsf/.
    bdv;hrefdl;fd;

--- WAV OCS AIRPORT EDITION/Assets/Scripts/levmun.cs
+++ WAV OCS AIRPORT EDITION/Assets/Scripts/levmun.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class levmun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+ 
+    public void OnclickLevel(int levelNum)
     {
-        
+        SceneManager.LoadScene("Gamescene");
     }
+    public void OnClickBack()
+    {
+    this.gameObject.SetActive(false);
+    }
+    // Start is called before the first
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
