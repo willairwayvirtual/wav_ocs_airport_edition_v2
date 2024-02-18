bdab57a2c37ba6654ef7c34504e84450c41c32bb
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2024-02-18 16:18:38 +0000

    gdsc;wegdsc;'redf;ll;gr
    bdc;lhfecdxg'ltvdh,';;;;'

---
+++ WAV OCS AIRPORT EDITION/Assets/Scripts/LevelObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LevelObject : MonoBehaviour
+{
+    public Button levelbutton;
+    public Image[] stars;
+
+}
