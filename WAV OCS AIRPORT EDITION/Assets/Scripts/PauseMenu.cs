e02217672c4a3991f03f47b122a653363beab136
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2024-03-12 09:06:39 +0000

    geqfjasfmf.mwqs
    rtgf.;u#6trghflp[,;l65trlhvfio;6rtkkkgwsrdfkvotweg0

--- WAV OCS AIRPORT EDITION/Assets/Scripts/PauseMenu.cs
+++ WAV OCS AIRPORT EDITION/Assets/Scripts/PauseMenu.cs
@@ -44,43 +44,8 @@ public class PauseMenu : MonoBehaviour
     }
     public void all_decks()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 10);
+        SceneManager.LoadScene("LevelSelectionMenu");
     }
-    public void all_from_deck1()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
-    }
-    public void all_from_deck2()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
-    }
-    public void all_from_deck3()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 6);
-    }
-    public void all_from_deck4()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 8);
-    }
-    public void all_from_deck5()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 10);
-    }
-    public void all_from_deck6()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 12);
-    }
-    public void all_from_deck7()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 14);
-    }
-    public void all_from_deck8()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 16);
-    }
-    public void all_from_deck9()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 18);
-    }
+  
 
 }
