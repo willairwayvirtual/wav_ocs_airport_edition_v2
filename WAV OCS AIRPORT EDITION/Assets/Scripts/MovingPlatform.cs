a6e3e6e47d9ad0a6f6a19f72ef4b2eb8c1954718
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2024-05-10 15:13:25 +0100

    fe,sgl;krwejimohnpketgdvcpk
    gewmdsv,.,tkgmef;dok;htegdvc iok[ l,;/.

--- WAV OCS AIRPORT EDITION/Assets/Scripts/MovingPlatform.cs
+++ WAV OCS AIRPORT EDITION/Assets/Scripts/MovingPlatform.cs
@@ -17,6 +17,8 @@ public class MovingPlatform : MonoBehaviour
 
     private float _timeToWaypoint;
     private float _elapsedTime;
+    public float waitDuration;
+    int speedMultiplier = 1;
 
 
     // Start is called before the first frame update
@@ -29,7 +31,7 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _elapsedTime += Time.deltaTime;
+        _elapsedTime += speedMultiplier * _Speed*Time.deltaTime;
 
         float elapsedPercentage = _elapsedTime / _timeToWaypoint;
         transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
@@ -50,5 +52,13 @@ public class MovingPlatform : MonoBehaviour
 
         float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
         _timeToWaypoint = distanceToWaypoint / _Speed;
+
+    }
+
+    IEnumerator WaitNextPoint()
+    {
+        speedMultiplier = 0;
+        yield return new WaitForSeconds(waitDuration);
+      speedMultiplier = 1;
     }
 }
