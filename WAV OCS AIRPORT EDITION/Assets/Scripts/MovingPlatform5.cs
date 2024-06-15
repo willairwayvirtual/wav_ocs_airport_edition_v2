a9966b17fd325f7f27c13f89f51388b0e3640228
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2024-06-15 10:52:35 +0100

    adfszltrfdlp;gfddsfr;df;lhtdflpg;rfdvcx,;
    nfglfhtdlpfcx; vptrhkofjkohbrs;;;;vdz;fdvcgfhv.trvsc.b;dv;# f

---
+++ WAV OCS AIRPORT EDITION/Assets/Scripts/MovingPlatform5.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform5 : MonoBehaviour
+{
+    [SerializeField]
+    private WaypointPath5 _waypointPath;
+
+    [SerializeField]
+    private float _Speed;
+
+    private int _targetWaypointIndex;
+
+    private Transform _previousWaypoint;
+    private Transform _targetWaypoint;
+
+    private float _timeToWaypoint;
+    private float _elapsedTime;
+    public float waitDuration;
+    int speedMultiplier = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetNextWaypoint();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapsedTime += speedMultiplier * _Speed * Time.deltaTime;
+
+        float elapsedPercentage = _elapsedTime / _timeToWaypoint;
+        transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
+
+        if (elapsedPercentage >= 1)
+        {
+            TargetNextWaypoint();
+        }
+    }
+
+    private void TargetNextWaypoint()
+    {
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+
+        _elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
+        _timeToWaypoint = distanceToWaypoint / _Speed;
+        StartCoroutine(WaitNextPoint());
+
+
+    }
+
+    IEnumerator WaitNextPoint()
+    {
+        speedMultiplier = 0;
+        yield return new WaitForSeconds(waitDuration);
+        speedMultiplier = 1;
+    }
+}
